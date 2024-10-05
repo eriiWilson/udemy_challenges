b95ddc4a5798ec50500c9e418899dc1aa82028ce
eriiWilson <eric_wils0n@outlook.com>
2024-10-05 13:15:41 -0300

    challange_udemy

---
+++ ex1.py
@@ -0,0 +1,11 @@
+# Create variables for Name, Last Name, Age, Year of Birth, Is Adult, and Height in meters
+# Then, print all of them
+
+name = 'eric'
+last_name = 'wilson'
+age = 20
+year_of_birth = 2004
+is_adult = age >= 18
+height = 1.79
+
+print(f'name: {name } {last_name}, age: {age}, year of birth: {year_of_birth}, is adult? {is_adult}, height: {height}')
---
+++ ex2.py
@@ -0,0 +1,8 @@
+# Calculate BMI and display in the terminal your name, your height, your weight, and your BMI
+
+name = 'eric wilson'
+height = 1.78
+weigth = 80
+bmi = weigth / (height * height)
+
+print(f'my name is {name} my height is {height} and my weight {weigth} so my bmi is {round(bmi , 2)}')
